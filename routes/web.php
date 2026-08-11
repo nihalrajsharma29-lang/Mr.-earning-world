d36fcc067bde40e2f901274ce83b425cad078810
Nihal Raj <Lifelineworld11@gmail.com>
2026-08-11 16:38:52 +0530

    Redirect root to login

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Simple sitemap for public pages. Update list as needed.
